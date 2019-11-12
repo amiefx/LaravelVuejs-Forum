3281caf99fe8f4c9e54e345c7d1fbe6c2838e9e8
Amie Fx <amiefx1901@gmail.com>
2019-11-12 12:22:24 +0500

    Relationship

--- app/Model/Reply.php
+++ app/Model/Reply.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Reply extends Model
 {
-    //
+    public function question()
+    {
+        return $this->belongsTo(Question::class);
+    }
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
