a847acc82a37ea58da897423613b2a1b7530cd10
Amie Fx <amiefx1901@gmail.com>
2019-11-24 23:42:32 +0500

    Helpers, class and functions - authentication

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,6 +5,11 @@ import vuetify from './vuetify';
 import App from './components/AppHome.vue';
 import router from './router/router.js';
 
+
+import User from './Helpers/User'
+window.User = User
+console.log(User.id())
+
 const app = new Vue({
     vuetify,
     render: h => h(App),
